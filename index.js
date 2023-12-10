29b0ca6bb4185fcbc56131453d2ad0fb2fe3534d
Mr.harrum <harrumjawed2222@gmail.com>
2023-12-10 19:29:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// var container = document.querySelector(".master-container")
+// var btn = document.querySelector(".cursor")
+// container.addEventListener("mousenter",function(){
+//     alert("working")
+// })
